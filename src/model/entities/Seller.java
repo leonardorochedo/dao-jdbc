a81b571939f9a98f9ccab1b05559b5598087b7ad
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2022-12-27 01:02:54 -0300

    Reusing instantiation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -7,16 +7,16 @@ import java.util.Objects;
 public class Seller implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer id;
 	private String name;
 	private String email;
 	private Date birthDate;
 	private Double baseSalary;
 	private Department department;
-	
+
 	public Seller() {
-		
+
 	}
 
 	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
@@ -98,5 +98,5 @@ public class Seller implements Serializable {
 		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
 				+ baseSalary + ", department=" + department + "]";
 	}
-	
+
 }
